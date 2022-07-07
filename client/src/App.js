0ed0ab80c770ae81349e781d78fc3f78d7ee592e
Emre Yasin Şallı <94170595+emreysalli@users.noreply.github.com>
2022-07-07 14:50:26 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,15 @@ function App() {
 
   socket.on("connect",()=>{
     setSocketId(socket.id);
+    console.log(`Socket connected: ${socket.connected}`);
+    console.log(`Socket disconnected: ${socket.disconnected}`);
+
+    // const engine=socket.io.engine;
+    // console.log(engine.transport.name);
+    // engine.once("upgrade", () => {
+    //   // called when the transport is upgraded (i.e. from HTTP long-polling to WebSocket)
+    //   console.log(engine.transport.name); // in most cases, prints "websocket"
+    // });
   });
 
   const joinRoom=()=>{
@@ -26,6 +35,13 @@ function App() {
     }
   };
 
+  const leaveRoom=()=>{
+    let id=socket.id;
+    socket.emit("leave_room",{id,roomNum},returnMsg=>{
+      alert(returnMsg);
+    });
+  };
+
   const sendMessage=()=>{
      socket.emit("send_message",{message,roomNum});
   };
@@ -34,7 +50,28 @@ function App() {
     socket.emit("broadcast",{message});
   };
 
+  const socketOpen=()=>{
+    socket.connect();
+    
+    setTimeout(() => {
+      alert(`Socket open ${socket.connected}`)
+    }, 500);
+
+  }
+
+  const socketClose=()=>{
+    socket.close();
+    alert(`Socket close ${socket.disconnected}`);
+  }
+
   // socket.timeout(5000).emit("send_message",{message,roomNum});
+  socket.onAny((event, ...args) => {
+    console.log(`got ${event}`);
+  });
+
+  socket.on("connect_error", (error) => {
+    console.log(`Connect error: ${error}`);
+  });
 
   useEffect(()=>{
     socket.on("receive_message",(data)=>{
@@ -47,10 +84,13 @@ function App() {
       <div style={{marginBottom:20}}>
         <input placeholder='room number' onChange={(event)=>{setRoomNum(event.target.value)}}/>
         <button onClick={joinRoom}>join room</button>
+        <button onClick={leaveRoom}>leave room</button>
       </div>
       <input placeholder="Message" onChange={(event)=>{setMessage(event.target.value)}}/>
       <button onClick={sendMessage}>Send</button>
       <button onClick={broadcast}>Broadcast</button>
+      <button onClick={socketOpen}>Socket Open</button>
+      <button onClick={socketClose}>Socket Close</button>
       <h3>Socket id:</h3>
       {socketId}
       <h1>Message:</h1>
