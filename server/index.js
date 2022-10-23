aed186e0616e1c18f3c13c59d208c0ecfbaae792
emresalli <94170595+emreysalli@users.noreply.github.com>
2022-10-23 22:23:36 +0300

    commit

--- server/index.js
+++ server/index.js
@@ -1,42 +1,42 @@
-const express= require("express");
-const app=express();
-const http=require("http");
-const {Server}=require("socket.io");
-const cors=require("cors");
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
 app.use(cors());
-const server=http.createServer(app);
+const server = http.createServer(app);
 
 
-const io=new Server(server,{
-    cors:{
-        origin:"http://localhost:3000",
-        methods:["GET","POST"],
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
     },
 });
 
-io.on("connection",(socket)=>{
+io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
 
-    socket.on("join_room",(data,callback)=>{
+    socket.on("join_room", (data, callback) => {
         socket.join(data);
         callback(`Joined ${data}`);
     })
 
-    socket.on("leave_room",(data,callback)=>{
+    socket.on("leave_room", (data, callback) => {
         io.in(data.id).socketsLeave(data.roomNum);
         console.log("leave room");
         callback(`Leaved ${data.roomNum}`);
     })
 
-    socket.on("send_message",(data)=>{
+    socket.on("send_message", (data) => {
         // console.log(data);
         // socket.broadcast.emit("receive_message",data);
-        socket.to(data.roomNum).emit("receive_message",data); //aynı odadaki diger clientlara mesaj gonder
+        socket.to(data.roomNum).emit("receive_message", data); //aynı odadaki diger clientlara mesaj gonder
     });
 
-    socket.on("broadcast",(data)=>{
-        socket.broadcast.emit("receive_message",data); // gecerli client haric diger butun clientlara gonderir
+    socket.on("broadcast", (data) => {
+        socket.broadcast.emit("receive_message", data); // gecerli client haric diger butun clientlara gonderir
         // io.emit("receive_message",data); //butun clientlara gonderir
     });
 
@@ -46,6 +46,6 @@ io.on("connection",(socket)=>{
 
 });
 
-server.listen(3001,()=>{
+server.listen(3001, () => {
     console.log("Server is running");
 });
